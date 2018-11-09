aec11906610ed0531ed9df4d3286523981ba80e5
Shikhat Karkee <shikhatkarkee@gmail.com>
2018-11-09 22:22:30 +0000

    description and show route added

--- v2/app.js
+++ v2/app.js
@@ -11,7 +11,8 @@ app.set("view engine", "ejs");
 //setting up schema
 var campgroundSchema = new mongoose.Schema({
     name:String,
-    image:String
+    image:String,
+    description:String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -22,36 +23,51 @@ app.get("/", function(req, res){
 });
 
 //camgrounds page
-app.get("/campgrounds", function(req, res){
+app.get("/index", function(req, res){
     //find all campgrounds from db and then render it
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err)
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         }
     })
     //
 });
 
 //posting campgrounds
-app.post("/campgrounds", function(req, res){
+app.post("/index", function(req, res){
     //get data from form and add it to campground database
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description:description};
     Campground.create(newCampground, function(err, createdCampground){
         if (err){
             console.log(err);
         } else {
-            res.redirect("/campgrounds");
+            res.redirect("/index");
         }
     });
 });
 
-//
-app.get("/campgrounds/new", function(req, res) {
+//Form to create new campground
+app.get("/index/new", function(req, res) {
     res.render("new"); 
+});
+
+//
+app.get("/index/:id", function(req, res) {
+//find campground with id
+Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+        console.log(err);
+    } else {
+        res.render("show", {campground:foundCampground});
+    }
+});
+
+//render found campground
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
